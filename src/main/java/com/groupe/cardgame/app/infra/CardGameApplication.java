dbc09e69326dd4a5d5390a9764099dc6d8c1d595
shelbon <mouclepatrick@protonmail.com>
2022-12-11 15:34:22 +0100

    refactor:update structure add package for infrastructure code

--- src/main/java/com/groupe/cardgame/app/infra/CardGameApplication.java
+++ src/main/java/com/groupe/cardgame/app/infra/CardGameApplication.java
@@ -1,4 +1,4 @@
-package com.groupe.cardgame.app;
+package com.groupe.cardgame.app.infra;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
